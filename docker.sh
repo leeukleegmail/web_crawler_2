990796b790c5312077261153df89d75ec6b42e80
lee <leeuklee@gmail.com>
2025-10-22 19:58:59 +0200

    first

--- docker.sh
+++ docker.sh
@@ -4,5 +4,5 @@ CONTAINER_PORT="5009"
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 docker build --tag $CONTAINER_NAME --build-arg container_name=$CONTAINER_NAME .
-docker run -d -p 5001:$CONTAINER_PORT --name $CONTAINER_NAME --restart unless-stopped -v $(pwd)/:/$CONTAINER_NAME $CONTAINER_NAME
+docker run -d -p 5000:$CONTAINER_PORT --name $CONTAINER_NAME --restart unless-stopped -v $(pwd)/:/$CONTAINER_NAME $CONTAINER_NAME
 
