a8ce2857231ab567ee4b6cfdfbcbb2b47015302e
lee <leeuklee@gmail.com>
2025-10-28 19:11:50 +0100

    first

--- docker.sh
+++ docker.sh
@@ -1,8 +1,8 @@
 CONTAINER_NAME="web_crawler_2"
-CONTAINER_PORT="5009"
+CONTAINER_PORT="5000"
 
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 docker build --tag $CONTAINER_NAME --build-arg container_name=$CONTAINER_NAME .
-docker run -d -p 5009:$CONTAINER_PORT --name $CONTAINER_NAME --restart unless-stopped -v $(pwd)/:/$CONTAINER_NAME $CONTAINER_NAME
+docker run -d -p 5000:$CONTAINER_PORT --name $CONTAINER_NAME --restart unless-stopped -v $(pwd)/:/$CONTAINER_NAME $CONTAINER_NAME
 
